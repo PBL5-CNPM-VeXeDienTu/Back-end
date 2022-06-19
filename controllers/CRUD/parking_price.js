44186bfce10ef7eaf4bf33761270c78723246267
Nguyễn Đặng Tuấn Kiệt <tuankietnk2001@gmail.com>
2022-06-19 11:14:08 +0700

    [Backend][API][Admin] Tạo API search cho trang danh sách vehicle

--- controllers/CRUD/parking_price.js
+++ controllers/CRUD/parking_price.js
@@ -6,7 +6,7 @@ const include = [
         include: [
             {
                 model: models.User,
-                attributes: { exclude: ['password', 'updatedAt'] },
+                attributes: { exclude: ['password', 'qr_key', 'updatedAt'] },
                 include: [
                     {
                         model: models.Role,
